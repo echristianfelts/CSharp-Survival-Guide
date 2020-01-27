144265b388af21709f030283ce9f8c6578fd4d1c
eric felts <echristianfelts@gmail.com>
2020-01-27 13:35:14 -0800

    Master Switch Statements Pt1.  Challenge Portion.

---
+++ Assets/Scripts/Switch_Intro.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch_Intro : MonoBehaviour
+{
+    public int selectedLevel;
+
+    private int _easy = 0;
+    private int _medium = 1;
+    private int _hard = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectedLevel = Random.Range(0,3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (selectedLevel)
+        {
+            case 0: //easy
+                Debug.Log("Easy..!");
+                break;
+            case 1: //medium
+                Debug.Log("Easy..!");
+                break;
+            case 2: //hard
+                Debug.Log("Easy..!");
+                break;
+            default:
+                Debug.Log("Invalid Selection..!!");
+                break;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            selectedLevel = Random.Range(0, 3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            selectedLevel = 55;
+        }
+    }
+}
---
+++ Assets/Scripts/Switch_Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch_Player : MonoBehaviour
+{
+    public int points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            points = 50;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            points = 100;
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            points = 0;
+        }
+
+
+        //based on the value of points...
+        // we print out.
+
+        if (points == 50)
+        {
+            Debug.Log("Points are 50!");
+        }
+        else if (points == 100)
+        {
+            Debug.Log("Points are 100!");
+        }
+        else
+            {
+            Debug.Log("Only socres of 50 or 100 get readouts..!");
+            }
+
+
+    }
+}
