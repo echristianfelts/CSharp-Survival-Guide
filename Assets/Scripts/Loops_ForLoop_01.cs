a7f18904061069482d7d17f322ec2253cdcf38ba
eric felts <echristianfelts@gmail.com>
2020-01-27 16:07:54 -0800

    For Loops Understanding Cont.

---
+++ Assets/Scripts/Loops_ForLoop_01.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops_ForLoop_01 : MonoBehaviour
+{
+
+    public string myName = "Chris";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for (int i = 0; i < 100; i++)  //'i' is like an index variable.
+        //{
+        //    Debug.Log(myName);
+
+        //}
+
+        //Debug.Log("Naming Loop is Finished...");
+
+        for ( int i = 0; i < 100; i++)  //'i' is like an index variable.
+        {
+            if (i % 2 == 0)
+            {
+                Debug.Log(i);
+            }
+
+        }
+
+        Debug.Log("Counting Loop is Finished...");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
