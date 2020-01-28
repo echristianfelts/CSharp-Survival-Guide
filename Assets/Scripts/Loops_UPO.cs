884d7428b018092d2304f6174bfee61fa00eac13
eric felts <echristianfelts@gmail.com>
2020-01-28 10:59:32 -0800

    UPO Challenge complete...

--- Assets/Scripts/Loops_UPO.cs
+++ Assets/Scripts/Loops_UPO.cs
@@ -11,7 +11,29 @@ public class Loops_UPO : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        for (int i = 0; i < 30; i++)  //'i' is like an index variable.
+        {
+            if (i < 11)
+            {
+                Debug.Log(i);
+
+            }
+            else if (i < 21 && (i % 2 == 0))
+            {
+                Debug.Log(i);
+
+            } else if (i>20 && (i % 2 == 1))
+            {
+                Debug.Log(i);
+
+            }
+
+
+
+
+        }
+
     }
 
     // Update is called once per frame
