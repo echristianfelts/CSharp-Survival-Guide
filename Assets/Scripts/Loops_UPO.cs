9fd6570e347d543d09de7d72ee58038d625ab03e
eric felts <echristianfelts@gmail.com>
2020-01-27 17:54:39 -0800

    added instructions.

--- Assets/Scripts/Loops_UPO.cs
+++ Assets/Scripts/Loops_UPO.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Loops_UPO : MonoBehaviour
 {
+    // create prog that prints out 0-10, then 10-20 evens, then 20-30 odds.
+    // hint modulus.
+
+
     // Start is called before the first frame update
     void Start()
     {
