48fdd5a08d1e9555ba91534b23fb700e7ea2fc90
eric felts <echristianfelts@gmail.com>
2020-01-29 10:36:29 -0800

    Access Array Elements

---
+++ Assets/Scripts/Array_Intro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array_Intro : MonoBehaviour
+{
+    public string[] names;
+
+    public string[] items = new string[5];
+
+    public int[] ages = new int[] {5, 3, 2, 55,66 };
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(names[0]);
+        Debug.Log(ages[3]);
+        Debug.Log(items[1]);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
