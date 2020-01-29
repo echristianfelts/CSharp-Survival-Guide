9c2cb33d9a93d4a499330451da0c70254b390953
eric felts <echristianfelts@gmail.com>
2020-01-29 15:25:18 -0800

    Arrays:Print Out all Elements using ForEach Loop

---
+++ Assets/Scripts/Array_ForEachLoop.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array_ForEachLoop : MonoBehaviour
+{
+    //public int[] itemID;
+    //public string[] itemName;
+    public int[] ageArray;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            /*for(int i = 0; i < itemName.Length; i++)
+            {
+                Debug.Log(itemName[i]);
+
+            }*/
+
+            foreach (var age in ageArray)
+            {
+
+                    Debug.Log(age);
+                
+
+            }
+        }
+
+    }
+}
+
---
+++ Assets/Scripts/Array_ForLoop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array_ForLoop : MonoBehaviour
+{
+    public int[] itemID;
+    public string[] itemName;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for (int i = 0; i < itemName.Length; i++)
+            {
+                if (itemName[i] == "sword")
+                {
+                    Debug.Log("Item Exists..!");
+                    Debug.Log("Item ID: " + itemID[i]);
+
+                }
+            }
+        }
+        
+    }
+}
