82d48f4e0174b5c4e7f4ff5e8a5aac3ab98f37aa
eric felts <echristianfelts@gmail.com>
2020-01-29 17:25:57 -0800

    Looping Practical Example - Start

---
+++ Assets/Scripts/Array_LoopThroughData.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public int itemID;
+    public string name;
+    public string description;
+}
+
+public class Array_LoopThroughData : MonoBehaviour
+{
+    public Item[] myItems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(var item in myItems)
+        {
+            Debug.Log(item.name);
+
+        }
+
+        foreach(var item in myItems)
+            if (item.itemID == 7)
+            {
+                Debug.Log("You have item number 7..!!! [FOREACH METHOD]");
+            }
+            else
+            {
+                Debug.Log("Sorry.  No soup for you..!!! [FOREACH METHOD]");
+            }
+
+        for (int i = 0; i < myItems.Length; i++)
+        {
+            if (myItems[i].itemID == 7)
+            { 
+            Debug.Log("You have item number 7..!!! [FOR METHOD]");
+            }
+            else
+            {
+            Debug.Log("You get nothing...!!!!  [FOR METHOD]");
+            }
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Space))
+        {
+            int randoID = Random.Range(0, myItems.Length);
+            {
+                Debug.Log(myItems[randoID].name);
+
+                //for (int i = 0; i < myItems.Length ;i++)
+                //{
+                //}
+
+            }
+
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/Array_PracticalExample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Array_PracticalExample : MonoBehaviour
+{
+    public GameObject[] cubes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach(var cube in cubes)
+            {
+                cube.GetComponent<MeshRenderer>().material.color = Color.red;
+
+            }
+
+        }
+    }
+}
