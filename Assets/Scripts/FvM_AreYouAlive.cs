55166ae695bda6e0a9b18e6b1f4d5820b0956a1e
eric felts <echristianfelts@gmail.com>
2020-02-07 12:49:00 -0800

    FvM Are you alive Challenge.  Solved.

--- Assets/Scripts/FvM_AreYouAlive.cs
+++ Assets/Scripts/FvM_AreYouAlive.cs
@@ -6,17 +6,36 @@ public class FvM_AreYouAlive : MonoBehaviour
 {
     //  Create a program that checke to see if the player is dead or not. When you hit the space-key, you should damage 
     //  the player by a random amount.  If the player is dead (health less than 1...), print out "The Player has died..."
+    private int _playerHealth = 100;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Player Health :"+ _playerHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerDamage();
+            if (_playerHealth < 1)
+            {
+                _playerHealth = 0;
+                Debug.Log("The Player has Died..!!!");
+                Debug.Log("Player Health :" + _playerHealth);
+            } else
+            {
+                Debug.Log("Player Health :" + _playerHealth);
+            }
+
+        }
+    }
+
+    private void PlayerDamage()
+    {
+        _playerHealth -= Random.Range(1, 6);
     }
 }
