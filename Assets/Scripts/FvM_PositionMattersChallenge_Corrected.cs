7c97d50db2f300fd2e1ff37f7784f60dbaed0258
eric felts <echristianfelts@gmail.com>
2020-02-07 15:24:45 -0800

    FvM Position Matters Challenge.Corrected.

---
+++ Assets/Scripts/FvM_PositionMattersChallenge_Corrected.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FvM_PositionMattersChallenge_Corrected : MonoBehaviour
+{
+
+    public Vector3[] positions;
+    private int _randomIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _randomIndex = GetRandom();
+
+        Debug.Log("Random Index: " + _randomIndex);
+
+        transform.position = GetPosition(_randomIndex);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int GetRandom()
+    {
+        return Random.Range(0, positions.Length);
+    }
+
+    Vector3 GetPosition(int index)
+    {
+        return positions[index];
+    }
+
+}
