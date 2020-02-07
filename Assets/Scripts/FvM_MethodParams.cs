72b6e692cd1ac5ba9420189b63607314760e4ade
eric felts <echristianfelts@gmail.com>
2020-02-06 19:06:03 -0800

    Method Parameters.

---
+++ Assets/Scripts/FvM_MethodParams.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FvM_MethodParams : MonoBehaviour
+{
+
+    public int health;
+
+    public void Damage(int damageAmount)
+    {
+
+        health -= damageAmount;
+
+        if (health < 1)
+        {
+            health = 0;
+            Destroy(this.gameObject);
+        }
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Damage(5);
+        }
+    }
+
+}
