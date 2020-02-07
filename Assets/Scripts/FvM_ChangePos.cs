db5e5152e65747309b93798641fa1dc580a68a4a
eric felts <echristianfelts@gmail.com>
2020-02-07 12:19:44 -0800

    Return functions.  git add .git add .git add .!

---
+++ Assets/Scripts/FvM_ChangePos.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FvM_ChangePos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Hard coded transform
+
+        // transform.position = new Vector3(0, 0, 0);
+        // ChangePosition(new Vector3(0, 0, 0));
+        transform.position = GetPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangePosition(Vector3 pos)
+    {
+        transform.position = pos;
+    }
+
+    public Vector3 GetPosition()
+    {
+        return Vector3.zero;
+    }
+
+    public Vector3 GetPosition_02(Vector3 pos)
+    {
+        pos = Vector3.zero;
+        return pos;
+    }
+
+    public Vector3 GetPosition_03(float x, float y, float z)
+    {
+        Vector3 pos = new Vector3( x, y, z);
+        return pos;
+    }
+}
---
+++ Assets/Scripts/FvM_ReturnTypeFunctions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FvM_ReturnTypeFunctions : MonoBehaviour
+{
+    public int total;
+    public int total2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        total = Sum(2, 5);
+        total2 = Sum(10, 24);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+}
