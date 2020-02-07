a6d86cd4e7478081dc7656139816c6f321df6857
eric felts <echristianfelts@gmail.com>
2020-02-06 16:42:05 -0800

    Intro to Function vs Method.

---
+++ Assets/Scripts/FvP_Intro_.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FvP_Intro_ : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start()");
+        MyMethod();
+        Debug.Log("Compleated calling MyMethod");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void MyMethod()
+    {
+        Debug.Log("MyMethod()");
+    }
+
+
+}
