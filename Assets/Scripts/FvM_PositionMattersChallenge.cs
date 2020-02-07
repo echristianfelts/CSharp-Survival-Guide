12c34c9be564d2633d32fd9d2e7f5fa0574852f9
eric felts <echristianfelts@gmail.com>
2020-02-07 13:47:05 -0800

    Position Matters Challenge Start

---
+++ Assets/Scripts/FvM_PositionMattersChallenge.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FvM_PositionMattersChallenge : MonoBehaviour
+{
+    //Create an array of 5 different positions and uses a custom method to generate a random index,
+    //then use a custom methodto set the position to that subindex...
+    //
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
