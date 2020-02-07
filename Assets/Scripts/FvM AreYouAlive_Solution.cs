96c313d7afcf09c67f66661e3ca96598ce4480a1
eric felts <echristianfelts@gmail.com>
2020-02-07 13:33:21 -0800

    Return Array Script.  KEY INFO..Git add .

---
+++ Assets/Scripts/FvM AreYouAlive_Solution.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FvMAreYouAlive_Solution : MonoBehaviour
+{
+
+    public int health = 100;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsDead()== false)
+        {
+            Damage(Random.Range(5,10));
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage(int damageAmount)
+    {
+        health -= damageAmount;
+
+        if (IsDead())
+        {
+            health = 0;
+            Debug.Log("The player has died..!!!");
+
+        }
+
+    }
+
+    public bool IsDead()
+    {
+        return health < 1;
+    }
+}
---
+++ Assets/Scripts/FvMReturnArray.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FvMReturnArray : MonoBehaviour
+{
+
+    /// <summary>
+    /// /This is good for getting a bunch of the same thing to all change in the same way...
+    /// There are probably other ways to filter other than "FindGameObjectsWithTag"
+    /// It's nice to have options...
+    /// </summary>
+
+    public GameObject[] players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            players = GetAllPlayers();
+        }
+    }
+
+    GameObject[] GetAllPlayers()
+    {
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+        
+        foreach(var p in allPlayers)
+        {
+            p.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
+
+        }
+
+        return allPlayers;
+    }
+
+}
