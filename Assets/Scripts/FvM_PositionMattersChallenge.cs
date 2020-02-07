0645e6376ed0f3f7f9a2fa619214328b8895b9f3
eric felts <echristianfelts@gmail.com>
2020-02-07 14:34:24 -0800

    Challenge: Position Matters.  My solution

--- Assets/Scripts/FvM_PositionMattersChallenge.cs
+++ Assets/Scripts/FvM_PositionMattersChallenge.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class FvM_PositionMattersChallenge : MonoBehaviour
 {
-    //Create an array of 5 different positions and uses a custom method to generate a random index,
+    //Create an array of 5 different positions 
+
+    public Vector3[] pos = new Vector3[5];
+    public GameObject[] gameObjectObject = new GameObject[5];
+    //and uses a custom method to generate a random index,
+
     //then use a custom methodto set the position to that subindex...
     //
 
@@ -17,6 +22,33 @@ public class FvM_PositionMattersChallenge : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+
+        {
+            for (int i = 0; i < pos.Length; i++)
+            {
+                if (gameObjectObject[i] == null)
+                {
+                    gameObjectObject[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                }
+
+                PutThingInRandomPlace(i);
+
+                gameObjectObject[i].GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
+                gameObjectObject[i].transform.position = pos[i];
+
+            }
+        }
+    }
+
+    public Vector3 GetPosition()
+    {
+        return new Vector3(Random.Range(-5,5), Random.Range(-5, 5), Random.Range(-5, 5));
+    }
+
+    void PutThingInRandomPlace(int x)
+    {
+        pos[x] = GetPosition();
     }
 }
