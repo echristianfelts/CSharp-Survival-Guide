8fb1dde9f0c213d1b7f70ebff94c791d04bcec1b
eric felts <echristianfelts@gmail.com>
2020-02-07 12:29:49 -0800

    FvM Are you alive Challenge.

---
+++ Assets/Scripts/FvM_AreYouAlive.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FvM_AreYouAlive : MonoBehaviour
+{
+    //  Create a program that checke to see if the player is dead or not. When you hit the space-key, you should damage 
+    //  the player by a random amount.  If the player is dead (health less than 1...), print out "The Player has died..."
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
