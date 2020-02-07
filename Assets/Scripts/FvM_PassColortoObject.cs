4f3aa2973dc9893274a9beae1c8cc6d09fd16e06
eric felts <echristianfelts@gmail.com>
2020-02-07 10:02:31 -0800

    Passcolor Fix

--- Assets/Scripts/FvM_PassColortoObject.cs
+++ Assets/Scripts/FvM_PassColortoObject.cs
@@ -18,13 +18,16 @@ public class FvM_PassColortoObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        PassColor();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PassColor();
+        }
     }
 
     private void PassColor()
