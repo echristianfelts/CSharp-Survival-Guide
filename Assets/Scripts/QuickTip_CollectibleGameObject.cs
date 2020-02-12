9aff85e708a0d22113d0adb8ca81af2d84ef14c4
eric felts <echristianfelts@gmail.com>
2020-02-12 09:59:43 -0800

    Collectible Game Objects

---
+++ Assets/Scripts/QuickTip_CollectibleGameObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickTip_CollectibleGameObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
