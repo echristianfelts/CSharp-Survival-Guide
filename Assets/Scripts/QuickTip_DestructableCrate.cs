be20827ba33f70fa74dc65fcf3d5245b44ff5d3b
eric felts <echristianfelts@gmail.com>
2020-02-12 20:00:56 -0800

    Most of Quick Tips.  (Exploding Crate still giving me trouble...)

---
+++ Assets/Scripts/QuickTip_DestructableCrate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickTip_DestructableCrate : MonoBehaviour
+{
+    public GameObject fracturedCrate;
+    public GameObject explosionEffect;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(fracturedCrate, transform.position, Quaternion.identity);
+            GameObject FracturedObj = Instantiate(explosionEffect, transform.position, Quaternion.identity);
+            Rigidbody[] allRigidBodies = fracturedCrate.GetComponentsInChildren<Rigidbody>();
+
+            //Debug.Log(allRigidBodies[1].name);
+            if (allRigidBodies.Length > 0)
+            {
+                // Debug.Log(allRigidBodies[1].name);
+                foreach (var body in allRigidBodies)
+                {
+                    Debug.Log(body.name);
+                    Debug.Log(body.transform.position);
+                    Debug.Log(transform.position);
+                    body.AddExplosionForce(5000, transform.position, 10);
+
+                }
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/QuickTip_LookRotation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickTip_LookRotation : MonoBehaviour
+{
+
+
+
+    [SerializeField]
+
+    private Transform _sphere;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //direction = destination - source
+        Vector3 directionToFace = _sphere.position - transform.position;
+        Debug.DrawRay(transform.position, directionToFace, Color.green);
+        //access current rot = Quantranion Look Rotation
+        //////  This snaps rotation to follow the target object...  transform.rotation = Quaternion.LookRotation(directionToFace);
+
+        //SlerpMods
+        Quaternion targetRotation = Quaternion.LookRotation(directionToFace);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
+
+
+    }
+}
