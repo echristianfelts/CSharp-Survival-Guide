7508530c9b603cf08b8b65ebad4061fb673e55d3
eric felts <echristianfelts@gmail.com>
2020-02-24 12:06:10 -0800

    Intro to Dictionaries

---
+++ Assets/Scripts/Dictionary_Intro_Database.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dictionary_Intro_Database : MonoBehaviour
+{
+
+    public List<Dictionary_Intro_Item> itemList = new List<Dictionary_Intro_Item>();
+
+    public Dictionary<int, Dictionary_Intro_Item> itemDictionary = new Dictionary<int, Dictionary_Intro_Item>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Dictionary_Intro_Item sword = new Dictionary_Intro_Item();
+        sword.name = "Sword";
+        sword.id = 0;
+
+        itemList.Add(sword);
+        itemDictionary.Add(0, sword);
+
+        var Item = itemDictionary[0];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
